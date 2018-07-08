bf4d53c5fcafa14dae6a3ea7714799f7c0625c72
Emily Wessel <ettl02@gmail.com>
2018-07-08 15:41:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,13 @@ attr_reader :s1, :s2, :s3
     @s3 = s3
   end
 
-def kind 
-  if (s1 * s2 * s3) == 0 || (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2
-    
-    raise TriangleError 
-  
-  end 
-  
+def kind
+  if (@s1 * @s2 * @s3) == 0 || (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2
+
+    raise TriangleError
+
+  end
+
   class TriangleError < StandardError
     # triangle error code
   end
