5252a31a3e418bd8a07250ed7f3a26262afea657
Emily Wessel <ettl02@gmail.com>
2018-07-08 15:40:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,21 @@
 class Triangle
 
 attr_reader :s1, :s2, :s3
-  
+
   def initialize (s1, s2, s3)
     @s1 = s1
     @s2 = s2
     @s3 = s3
+  end
+
+def kind 
+  if (s1 * s2 * s3) == 0 || (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2
+    
+    raise TriangleError 
+  
   end 
-
-
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
